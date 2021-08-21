77348a25c3cf002af73b0b2364893b93279f3fc2
hun756 <ekemenms@hotmail.com>
2021-08-21 17:22:49 +0300

    Add Big Integer

--- include/big_integer.hpp
+++ include/big_integer.hpp
@@ -1,9 +1,28 @@
 #ifndef BIG_INTEGER_HPP
 #define BIG_INTEGER_HPP
 
+#include <string>
+#include <sstream>
+#include <vector>
+
 namespace Pw
 {
-    class BigInteger {};
+    class BigInteger
+    {
+    public:
+        /**
+         * @brief Construct a new Big Integer object
+         * 
+         * @param str 
+         */
+        BigInteger(std::string str);
+
+
+        std::vector<int> value() const;
+
+    private:
+        std::vector<int> data; 
+    };
 } // namespace Pw
 
 
--- src/big_integer.cpp
+++ src/big_integer.cpp
@@ -1,1 +1,18 @@
 #include "big_integer.hpp"
+
+Pw::BigInteger::BigInteger(std::string str)
+    :   data({})
+{
+    std::stringstream ss(str);
+
+    char numChr;
+    while (ss >> numChr)
+        data.push_back(numChr - '0');
+}
+
+
+
+std::vector<int> Pw::BigInteger::value() const
+{
+    return data;
+}
