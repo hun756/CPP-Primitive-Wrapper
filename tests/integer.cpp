02bc4e46f3b810e4c78710751e28545620da442d
hun756 <ekemenms@hotmail.com>
2021-08-21 13:51:33 +0300

    Add Integer Wrapper Test
        -  Checked parse method

--- tests/integer.cpp
+++ tests/integer.cpp
@@ -1,1 +1,62 @@
 #include <gtest/gtest.h>
+#include "integer.hpp"
+
+
+TEST(ParseFromString, Integer)
+{
+    Pw::Integer p;
+
+    p.parse(std::string("-45"));
+    ASSERT_EQ(p.value(), -45);
+
+    p.parse(std::string("30"));
+    ASSERT_EQ(p.value(), 30);
+
+    p.parse(std::string("4321"));
+    ASSERT_EQ(p.value(), 4321);
+}
+
+
+TEST(Integer, ParseFromString)
+{
+    Pw::Integer p;
+
+    p.parse(std::string("-45"));
+    ASSERT_EQ(p.value(), -45);
+
+    p.parse(std::string("30"));
+    ASSERT_EQ(p.value(), 30);
+
+    p.parse(std::string("4321"));
+    ASSERT_EQ(p.value(), 4321);
+}
+
+
+TEST(Integer, ParseFromCstyleString)
+{
+    Pw::Integer p;
+
+    p.parse("-45");
+    ASSERT_EQ(p.value(), -45);
+
+    p.parse("30");
+    ASSERT_EQ(p.value(), 30);
+
+    p.parse("4321");
+    ASSERT_EQ(p.value(), 4321);
+}
+
+
+TEST(Integer, ParseFromFloat)
+{
+    Pw::Integer p;
+
+    p.parse(-45.8f);
+    ASSERT_EQ(p.value(), -45);
+
+    p.parse(30.9999f);
+    ASSERT_EQ(p.value(), 30);
+
+    p.parse(4321.0999f);
+    ASSERT_EQ(p.value(), 4321);
+}
