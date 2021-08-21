8d214c1c2918100591e272ec33d16c39d144e38b
hun756 <ekemenms@hotmail.com>
2021-08-21 13:51:09 +0300

    Add Integer Wrapper
        -  Implement basic lifetime
        -  Convert Integer to byte array
        -  Parse integer from another types.
        -  Convert Integer To Hex
        -  Convert Integer To String

--- include/integer.hpp
+++ include/integer.hpp
@@ -1,5 +1,95 @@
 #ifndef INTEGER_HPP
 #define INTEGER_HPP
 
+#include <string>
+#include <sstream>
+#include <limits>
+#include <memory>
+
+namespace Pw 
+{
+    class Integer
+    {
+    public:
+        /* Life Time Management */
+
+        /**
+         *  @brief Construct a new Integer object
+         * 
+        **/
+        Integer() = default;
+
+
+        /**
+         * @brief Construct a new Integer object
+         * 
+        **/
+        Integer(int);
+
+
+        /**
+         * @brief 
+         * 
+         * @return int 
+         */
+        operator int();
+
+
+        /**
+         * @brief 
+         * 
+         * @tparam T 
+         * @param pVal 
+        **/
+        template<typename T>
+        void parse(T pVal)
+        {
+            std::stringstream ss;
+            ss << pVal;
+
+            ss >> data;
+        }
+
+
+        /**
+         * @brief 
+         * 
+         * @return std::string 
+        **/
+        std::string toString() const;
+
+
+        /**
+         * @brief 
+         * 
+         * @return int 
+         */
+        int value() const;
+
+
+        /**
+         * @brief 
+         * 
+         * @return std::string 
+        **/
+        std::string toHexString();
+
+
+        /**
+         * @brief 
+         * 
+         * @return std::unique_ptr<std::byte> 
+         */
+        std::unique_ptr<std::byte[]> toByteArray();
+
+    public:
+        /* Constants */
+        const static int Min = std::numeric_limits<int>::min();
+        const static int Max = std::numeric_limits<int>::max();
+
+    private:
+        int data;
+    };
+}
 
 #endif /* end of include guard :  INTEGER_HPP */
--- src/integer.cpp
+++ src/integer.cpp
@@ -1,1 +1,46 @@
 #include "integer.hpp"
+
+Pw::Integer::Integer(int val) 
+{
+
+}
+
+
+Pw::Integer::operator int() 
+{
+    return data;
+}
+
+
+std::string Pw::Integer::toString() const
+{
+    return std::to_string(data);
+}
+
+
+int Pw::Integer::value() const
+{
+    return data;
+}
+
+
+std::string Pw::Integer::toHexString() 
+{
+    std::stringstream ss;
+
+    ss << std::hex << data;
+
+    return ss.str();
+}
+
+
+std::unique_ptr<std::byte[]> Pw::Integer::toByteArray() 
+{
+    std::unique_ptr<std::byte[]> byteArray(new std::byte[4]);
+
+     for (int i = 0; i < 4; i++)
+         byteArray[3 - i] = std::byte(data >> (i * 8));
+     return byteArray;
+
+    return byteArray;
+}
